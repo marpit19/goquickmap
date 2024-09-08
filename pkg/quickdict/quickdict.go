f59eb2223e3049b784b21d34bf1b0d30e3e67990
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2024-09-08 13:53:28 +0530

    implemented quicdict

---
+++ pkg/quickdict/quickdict.go
@@ -0,0 +1,55 @@
+package quickdict
+
+import (
+	"github.com/marpit19/goquickmap/pkg/quickmap"
+)
+
+// QuickDict represnts a dictionary data strucutre
+type QuickDict struct {
+	data *quickmap.QuickMap
+}
+
+// New creates and returns a new QuickDict
+func New() *QuickDict {
+	return &QuickDict{
+		data: quickmap.New(),
+	}
+}
+
+// Set inserts or updates a key-value pair in the dictionary
+func (d *QuickDict) Set(key string, value interface{}) {
+	d.data.Insert(key, value)
+}
+
+// Get retrieves a value by key from the dictionary
+func (d *QuickDict) Get(key string) (interface{}, bool) {
+	return d.data.Get(key)
+}
+
+// Delete removes a key-value pair from the dictionary
+func (d *QuickDict) Delete(key string) {
+	d.data.Delete(key)
+}
+
+// Size returns the number of key-value pairs in the dictionary
+func (d *QuickDict) Size() int {
+	return d.data.Size()
+}
+
+// Keys returns a slice of all keys in the dictionary
+func (d *QuickDict) Keys() []string {
+	keys := make([]string, 0, d.Size())
+	d.data.ForEach(func(key string, value interface{}) {
+		keys = append(keys, key)
+	})
+	return keys
+}
+
+// Values returns a slice of all values in the dictionary
+func (d *QuickDict) Values() []interface{} {
+	values := make([]interface{}, 0, d.Size())
+	d.data.ForEach(func(key string, value interface{}) {
+		values = append(values, value)
+	})
+	return values
+}
